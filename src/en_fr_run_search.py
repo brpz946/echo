44bf065e29078afb8ce0ea89e526848bf4cc0bbd
paiforsyth <paiforsyth@hotmail.com>
2018-02-04 21:46:54 -0500

    slight update to run scropt

--- src/en_fr_run_search.py
+++ src/en_fr_run_search.py
@@ -1,6 +1,8 @@
+import torch
 import logging
 import lang
 import manage
+cuda=torch.cuda.is_available()
 man = manage.Manager.basic_search_from_file(
     path="../data/eng-fra_tut/eng-fra.txt",
     report_interval=1000,
@@ -9,8 +11,8 @@ man = manage.Manager.basic_search_from_file(
     loglevel=logging.DEBUG,
     batchsize=32,
     testphrase="They are great.",
-    cuda=True,
+    cuda=cuda,
     hidden_dim=512,
     filt=lang.filter_pair_tut,
-    opt='rmsprop')
+    opt='rmsprop',validate=True)
 man.trainer.train(70000)
